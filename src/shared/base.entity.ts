a50a34a7f0a65858709e8e9bc369fa30e29cd2be
Lucas-Steffen <lucasgabriel.programador@gmail.com>
2026-08-24 01:31:55 -0400

    feat: add users module with controller, service, and entity for user management

---
+++ src/shared/base.entity.ts
@@ -0,0 +1,26 @@
+import { CreateDateColumn, 
+         DeleteDateColumn, 
+         PrimaryGeneratedColumn, 
+         UpdateDateColumn 
+        } from "typeorm";
+
+export abstract class baseEntity {
+    @PrimaryGeneratedColumn("uuid")
+    id!: string;
+
+    @CreateDateColumn({
+        type: 'timestamptz'
+    })
+    public createdAt!: Date;
+
+    @UpdateDateColumn({
+        type: 'timestamptz'
+    })
+    public updatedAt!: Date;
+
+    @DeleteDateColumn({
+        type: 'timestamptz',
+        nullable: true
+    })
+    public deletedAt!: Date;
+}
---
+++ src/users/models/user.entity.ts
@@ -0,0 +1,26 @@
+import { baseEntity } from "src/shared/base.entity";
+import { Column, Entity } from "typeorm";
+
+@Entity({ schema: 'public', name: 'users' })
+export class users extends baseEntity {
+    @Column({
+        type: 'varchar',
+        length: 255,
+        nullable: false
+    })
+    name!: string;
+
+    @Column({
+        type: 'varchar',
+        length: 255,
+        nullable: false
+    })
+    email!: string;
+
+    @Column({
+        type: 'varchar',
+        length: 72, // BCrypt truncates input after 72 characters, so anything beyond that doesn't affect the hash
+        nullable: false
+    })
+    password!: string
+}
